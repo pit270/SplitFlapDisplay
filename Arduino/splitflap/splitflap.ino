c75748ac9e751862ce9ff7e9c070c3b30c72bf8a
Chris Weeks <chrisrweeks@aol.com>
2022-11-30 23:32:35 -0600

    95% accuracy. Added 3 special chars

--- Arduino/splitflap/splitflap.ino
+++ Arduino/splitflap/splitflap.ino
@@ -31,11 +31,11 @@
 
 // Servos
 // pulse lengths out of 4096
-#define SERVO_ON_PULSE_LENGTH_FAST 364
-#define SERVO_ON_PULSE_LENGTH_SLOW 321
-#define SERVO_OFF_PULSE_LENGTH 307
-#define SERVO_RPM_FAST 63.83f // todo: calibrate this. May  need to calibrate for each servo
-#define SERVO_RPM_SLOW 41.0f
+#define SERVO_ON_PULSE_LENGTH_FAST 321
+#define SERVO_ON_PULSE_LENGTH_SLOW 314
+#define SERVO_OFF_PULSE_LENGTH 300
+#define SERVO_RPM_FAST 72.00f // todo: calibrate this. May  need to calibrate for each servo
+#define SERVO_RPM_SLOW 59.40f
 // how far does servo move when on, measured in counts per loop
 #define SERVO_DELTA_OMEGA_FAST (unsigned int)(SERVO_RPM_FAST * LOOP_TIME * COUNTS_PER_REV / 60000)
 #define SERVO_DELTA_OMEGA_SLOW (unsigned int)(SERVO_RPM_SLOW * LOOP_TIME * COUNTS_PER_REV / 60000)
@@ -45,45 +45,91 @@
 #define HE_TRIGGER_VOLTAGE 512 // TODO: calibrate this. What voltage to use to detect HE falling edge
 
 
+enum ServoState
+{
+  E_OFF = 0,
+  E_START,
+  E_SLOW,
+  E_FAST,
+};
+
 // Structs
 typedef struct
 {
-    int adcID;                      // which ADC is the HE connected to. -1: Arduino built in
-    unsigned int adcPin;            // which pin of the ADC is this HE connected to
+    unsigned char adcID;                      // which ADC is the HE connected to. -1: Arduino built in
+    unsigned char adcPin;            // which pin of the ADC is this HE connected to
     unsigned int HEPositionOffset;  // offset in counts for where HE and magnet cross relative to defined zero position (such as A or blank flap)
-    unsigned int last_value;
-    unsigned int value;
+    unsigned short last_value;
+    unsigned short value;
 } HallEffect;
+
+// typedef struct
+// {
+//     unsigned int pwmPin;            // which pwm pin this servo is connected to
+//     unsigned int position;          // position mod COUNTS_PER_REV
+//     unsigned int targetPosition;
+//     ServoState state;
+//     unsigned int startupCounter;
+//     float targetRPM;
+// } Servo;
 
 typedef struct
 {
-    unsigned int pwmPin;            // which pwm pin this servo is connected to
-    unsigned int position;          // position mod COUNTS_PER_REV
-    unsigned int targetPosition;
-    unsigned int state;             // 0=off, 1=slow, 2=fast
-    float targetRPM;
+  unsigned short pulseLengthOff;
+  unsigned short pulseLengthFast;
+  unsigned short pusleLengthSlow;
+  float rpmFast;
+  float rpmSlow;
+} ServoCal;
+
+ServoCal servoCals[NUM_MODULES] = 
+{
+  {300, 321, 314, 72.00f, 59.40f},
+  {295, 321, 310, 60.60f, 42.67f},
+  {300, 321, 314, 72.00f, 59.40f},
+  {300, 321, 314, 72.00f, 59.40f},
+  {300, 321, 314, 72.00f, 59.40f},
+  {300, 321, 314, 72.00f, 59.40f},
+  {300, 321, 314, 72.00f, 59.40f},
+  {300, 321, 314, 72.00f, 59.40f},
+  {300, 321, 314, 72.00f, 59.40f},
+  {300, 321, 314, 72.00f, 59.40f},
+  {300, 321, 314, 72.00f, 59.40f},
+  {300, 321, 314, 72.00f, 59.40f},
+};
+
+typedef struct
+{
+  unsigned int pwmPin;            // which pwm pin this servo is connected to
+  unsigned int position;          // position mod COUNTS_PER_REV
+  unsigned int targetPosition;
+  ServoState state;
+  unsigned int startPosition;
+  float targetRPM;
+  float lastTargetRpm;
+  ServoCal *cal;
 } Servo;
 
 typedef struct
 {
-    HallEffect hallEffect;
-    Servo servo;
+  HallEffect hallEffect;
+  Servo servo;
 } SFModule;
 
 SFModule sfModules[NUM_MODULES] = 
 {
-    {{-1, 0, 0, 0.0f, 0.0f}, {0, 0, 0, 0, 0}},       // {HE{adcID, adcPin, offset, 0, 0}, Servo{pwmPin, 0, 0, 0}}
-    {{0, 1, 0, 0.0f, 0.0f}, {1, 0, 0, 0, 0}},
-    {{0, 2, 0, 0.0f, 0.0f}, {2, 0, 0, 0, 0}},
-    {{0, 3, 0, 0.0f, 0.0f}, {3, 0, 0, 0, 0}},
-    {{0, 4, 0, 0.0f, 0.0f}, {4, 0, 0, 0, 0}},
-    {{0, 5, 0, 0.0f, 0.0f}, {5, 0, 0, 0, 0}},
-    {{0, 6, 0, 0.0f, 0.0f}, {6, 0, 0, 0, 0}},
-    {{0, 7, 0, 0.0f, 0.0f}, {7, 0, 0, 0, 0}},
-    {{1, 0, 0, 0.0f, 0.0f}, {8, 0, 0, 0, 0}},
-    {{1, 1, 0, 0.0f, 0.0f}, {9, 0, 0, 0, 0}},
-    {{1, 2, 0, 0.0f, 0.0f}, {10, 0, 0, 0, 0}},
-    {{1, 3, 0, 0.0f, 0.0f}, {11, 0, 0, 0, 0}},
+    {{0, 0, 0, 0, 0}, {0,  0, 0, 0, 0, 0, 0, &servoCals[1]}},       // {HE{adcID, adcPin, offset, 0, 0}, Servo{pwmPin, 0, 0, 0, 0, 0, calAddr}}
+    {{0, 1, 0, 0, 0}, {1,  0, 0, 0, 0, 0, 0, &servoCals[1]}},
+    {{0, 2, 0, 0, 0}, {2,  0, 0, 0, 0, 0, 0, &servoCals[2]}},
+    {{0, 3, 0, 0, 0}, {3,  0, 0, 0, 0, 0, 0, &servoCals[3]}},
+    {{0, 4, 0, 0, 0}, {4,  0, 0, 0, 0, 0, 0, &servoCals[4]}},
+    {{0, 5, 0, 0, 0}, {5,  0, 0, 0, 0, 0, 0, &servoCals[5]}},
+    {{0, 6, 0, 0, 0}, {6,  0, 0, 0, 0, 0, 0, &servoCals[6]}},
+    {{0, 7, 0, 0, 0}, {7,  0, 0, 0, 0, 0, 0, &servoCals[7]}},
+    {{1, 0, 0, 0, 0}, {8,  0, 0, 0, 0, 0, 0, &servoCals[8]}},
+    {{1, 1, 0, 0, 0}, {9,  0, 0, 0, 0, 0, 0, &servoCals[9]}},
+    {{1, 2, 0, 0, 0}, {10, 0, 0, 0, 0, 0, 0, &servoCals[10]}},
+    {{1, 3, 0, 0, 0}, {11, 0, 0, 0, 0, 0, 0, &servoCals[11]}},
 };
 
 
@@ -96,9 +142,12 @@ bool newMessageReceived = false;
 int debug = 0;
 
 // Function Definitions
-int calculatePWM(float rpm)
+int calculatePWM(float rpm, ServoCal *cal)
 {
-  return ((SERVO_ON_PULSE_LENGTH_FAST - SERVO_ON_PULSE_LENGTH_SLOW) / (SERVO_RPM_FAST - SERVO_RPM_SLOW)) * (rpm - SERVO_RPM_SLOW) + SERVO_ON_PULSE_LENGTH_SLOW;
+  if(rpm == 0) return cal->pulseLengthOff;
+  if(cal->rpmFast == cal->rpmSlow) return cal->pusleLengthSlow;
+
+  return ((cal->pulseLengthFast - cal->pusleLengthSlow) / (cal->rpmFast - cal->rpmSlow)) * (rpm - cal->rpmSlow) + cal->pusleLengthSlow;
 }
 
 
@@ -114,87 +163,125 @@ int ADC_Read(unsigned int adcId, unsigned int adcPin) {
 void update() {
   SFModule *sfm;
 
-    if(newMessageReceived)
+  if(newMessageReceived)
+  {
+      for(int i = 0; i < NUM_MODULES; ++i)
+      {
+          // interpret period as 'module is behind by one, advance it by one letter without changing final position'
+          if(message[i] == '.')
+          {
+            sfModules[i].servo.position = (sfModules[i].servo.position - COUNTS_PER_FLAP) % COUNTS_PER_REV;
+            continue;
+          }
+          // interpret comma as 'module is ahead by one, loop around to get to previous letter'
+          else if(message[i] == ',')
+          {
+            sfModules[i].servo.position = (sfModules[i].servo.position + COUNTS_PER_FLAP) % COUNTS_PER_REV;
+            continue;
+          }
+          // interpret slash as do nothing
+          else if(message[i] == '/')
+          {
+            continue;
+          }
+          // assuming blank flap is at position 0, target postion is cacluated as follows
+          sfModules[i].servo.targetPosition = message[i] * COUNTS_PER_FLAP;
+      }
+      newMessageReceived = false; // UNCOMMENT
+  }
+
+  // for(int i = 0; i < NUM_MODULES; ++i)
+  for(int i = 0; i < 1; ++i)
+  {
+    sfm = &sfModules[i];
+
+    // update HE value
+    sfm->hallEffect.last_value = sfm->hallEffect.value;
+    sfm->hallEffect.value = ADC_Read(sfm->hallEffect.adcID, sfm->hallEffect.adcPin);
+    
+    // update position
+    sfm->servo.position = (sfm->servo.position + SERVO_DELTA_OMEGA(sfm->servo.targetRPM)) % COUNTS_PER_REV;
+
+    // check for HE falling edge
+    if(sfm->hallEffect.last_value > HE_TRIGGER_VOLTAGE 
+        && sfm->hallEffect.value < HE_TRIGGER_VOLTAGE)
     {
-        for(int i = 0; i < NUM_MODULES; ++i)
-        {
-            // assuming blank flap is at position 0, target postion is cacluated as follows
-            sfModules[i].servo.targetPosition = message[i] * COUNTS_PER_FLAP;
-        }
-        newMessageReceived = false;
+        sfm->servo.position = sfm->hallEffect.HEPositionOffset;
     }
 
-    for(int i = 0; i < NUM_MODULES; ++i)
+    // calculate new target rpm
+    sfm->servo.lastTargetRpm = sfm->servo.targetRPM;
+    if(sfm->servo.position >= sfm->servo.targetPosition && sfm->servo.position < sfm->servo.targetPosition + (COUNTS_PER_FLAP / 4))
     {
-        sfm = &sfModules[i];
-        // update HE value
-        sfm->hallEffect.last_value = sfm->hallEffect.value;
-        sfm->hallEffect.value = ADC_Read(sfm->hallEffect.adcID, sfm->hallEffect.adcPin);
-        // update servo position and time calculations and servo status
-        if(sfm->servo.position >= sfm->servo.targetPosition && sfm->servo.position < sfm->servo.targetPosition + (COUNTS_PER_FLAP / 4))
-        {
-            // stop servo
-            // to avoid sending too many unneccesary i2c messages, only send if servo is not already off
-            if(sfm->servo.state > 0)
-            {
-                // printf("stopping servo %d\n", i);
-                // pwmWrite(PWM_PIN(sfm->servo.pwmPin), SERVO_OFF_PULSE_WIDTH);
-                interrupts();
-                pwm.setPin(sfm->servo.pwmPin, 0);
-                noInterrupts();
-                sfm->servo.state = 0;
-                sfm->servo.targetRPM = 0;
-            }
-        }
-        else if((sfm->servo.targetPosition - sfm->servo.position) % COUNTS_PER_REV < COUNTS_PER_FLAP * 8)
-        {
-          sfm->servo.targetRPM -= 0.5;
-          if(sfm->servo.targetRPM < SERVO_RPM_SLOW)
-          {
-            sfm->servo.targetRPM = SERVO_RPM_SLOW;
-          }
-          if(sfm->servo.state != 1)
-          {
-            interrupts();
-            pwm.setPin(sfm->servo.pwmPin, calculatePWM(sfm->servo.targetRPM));
-            noInterrupts();
-            sfm->servo.state = 1;
-          }
-          else
-          {
-            sfm->servo.position = (sfm->servo.position + SERVO_DELTA_OMEGA(sfm->servo.targetRPM)) % COUNTS_PER_REV;
-          }
-        }
-        else
-        {
-            // to avoid sending too many unneccesary i2c messages, only send if servo is not already on
-            if(sfm->servo.state < 2)
-            {
-                // move servo
-                // printf("moving servo %d\n", i);
-                // pwmWrite(PWM_PIN(sfm->servo.pwmPin), SERVO_ON_PULSE_WIDTH);
-                interrupts();
-                pwm.setPin(sfm->servo.pwmPin, SERVO_ON_PULSE_LENGTH_FAST);
-                noInterrupts();
-                sfm->servo.state = 2;
-                sfm->servo.targetRPM = SERVO_RPM_FAST;
-            }
-            else    // only update position after code has looped at least once
-            {
-                sfm->servo.position = (sfm->servo.position + SERVO_DELTA_OMEGA_FAST) % COUNTS_PER_REV;
-            }
-        }
-        // check for HE falling edge
-        if(sfm->hallEffect.last_value > HE_TRIGGER_VOLTAGE 
-            && sfm->hallEffect.value < HE_TRIGGER_VOLTAGE)
-        {
-            sfm->servo.position = sfm->hallEffect.HEPositionOffset;
-        }
+      sfm->servo.targetRPM = 0;
+      sfm->servo.state = E_OFF;
     }
+    else
+    {
+      // startup
+      // if(sfm->servo.state == E_OFF)
+      // {
+      //   sfm->servo.targetRPM = sfm->servo.cal->rpmFast;
+      //   sfm->servo.startPosition = sfm->servo.position;
+      //   sfm->servo.state = E_START;
+      // }
+      // else if(sfm->servo.position >= sfm->servo.startPosition && sfm->servo.position < sfm->servo.startPosition + (COUNTS_PER_FLAP * 2))
+      // {
+      //   sfm->servo.targetRPM = sfm->servo.cal->rpmFast;
+      // }
+      if(sfm->servo.state == E_OFF)
+      {
+        sfm->servo.targetRPM = sfm->servo.cal->rpmSlow;
+        sfm->servo.state = E_START;
+      }
+      if(sfm->servo.state == E_START)
+      {
+        sfm->servo.targetRPM += 0.1;
+        if(sfm->servo.targetRPM > sfm->servo.cal->rpmFast)
+        {
+          sfm->servo.targetRPM = sfm->servo.cal->rpmFast;
+          sfm->servo.state = E_FAST;
+        }
+      }
+
+      // slow down
+      else if((sfm->servo.targetPosition - sfm->servo.position) % COUNTS_PER_REV < COUNTS_PER_FLAP * 8)
+      {
+        sfm->servo.targetRPM -= 0.1;
+        if(sfm->servo.targetRPM < sfm->servo.cal->rpmSlow)
+        {
+          sfm->servo.targetRPM = sfm->servo.cal->rpmSlow;
+        }
+        sfm->servo.state = E_SLOW;
+      }
+
+      // middle
+      else
+      {
+        sfm->servo.targetRPM = sfm->servo.cal->rpmFast;
+        sfm->servo.state = E_FAST;
+      }
+    }
+
+    // send updates to servo
+    if(sfm->servo.targetRPM != sfm->servo.lastTargetRpm)
+    {
+      // Serial.print(sfm->servo.targetRPM);
+      // Serial.print(", ");
+      // Serial.println(calculatePWM(sfm->servo.targetRPM, sfm->servo.cal));
+      interrupts();
+      pwm.setPin(sfm->servo.pwmPin, calculatePWM(sfm->servo.targetRPM, sfm->servo.cal));
+      noInterrupts();
+    }
+  }
 }
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  Wire.setClock(100000);
+  Wire.begin();
 
   cli(); // disable interrupts
 
@@ -208,13 +295,17 @@ void setup() {
   OCR1A = 10000; // interrupt when timer reaches 10000 ticks
   sei(); // enable interrupts
 
-  Serial.begin(9600);
 
   pwm.begin();
   pwm.setOscillatorFrequency(PWM_CLK_FREQ);
   pwm.setPWMFreq(PWM_FREQ);
 
   delay(10);
+
+  for(int i = 0; i < NUM_MODULES; ++i)
+  {
+    pwm.setPin(sfModules[i].servo.pwmPin, sfModules[i].servo.cal->pulseLengthOff);
+  }
 }
 
 void GetNewMessage() {
@@ -249,6 +340,11 @@ void GetNewMessage() {
     {
         newMessage[i] = nextChar - 96;
     }
+    else if(nextChar == ',' || nextChar == '.' || nextChar == '/')
+    {
+      // do nothing
+      newMessage[i] = nextChar;
+    }
     else
     {
         // either a proper space, or an invalid character, set to space for both
@@ -267,10 +363,10 @@ void GetNewMessage() {
 void loop() {
   // put your main code here, to run repeatedly:
   GetNewMessage();
-  // Serial.println(sfModules[0].servo.position);
+  Serial.println(sfModules[0].servo.position);
   // Serial.println(sfModules[0].servo.targetPosition);
   // Serial.println(sfModules[0].servo.isOn);
-  Serial.println(SERVO_DELTA_OMEGA_FAST);
+  // Serial.println(SERVO_DELTA_OMEGA_FAST);
   delay(1000);
 }
 
